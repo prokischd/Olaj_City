e63ad66475ca7c64ab3666bdce57dfa59da7d77f
prokischd <prokischd@gmail.com>
2019-11-30 18:28:23 +0100

    level demo more changes

---
+++ Assets/Code/Dani_Code/Random_Tree_Sprite.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Random_Tree_Sprite : MonoBehaviour
+{
+
+public Sprite[] allSprites;
+private SpriteRenderer myRenderer;
+
+    void Start()
+    {
+        myRenderer=GetComponent<SpriteRenderer>();
+        myRenderer.sprite=allSprites[Random.Range(0,allSprites.Length-1)];
+    }
+
+    void Update()
+    {
+        
+    }
+}
