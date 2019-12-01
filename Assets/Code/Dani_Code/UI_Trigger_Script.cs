242ee72e009f5b6701222721be5370b118814d79
prokischd <prokischd@gmail.com>
2019-12-01 10:31:26 +0100

    animation trigger implemented

---
+++ Assets/Code/Dani_Code/UI_Trigger_Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Trigger_Script : MonoBehaviour
+{
+    public GameObject uiObj;
+    void OnTriggerEnter2D(Collider2D col){
+        Debug.Log(col.name.ToString());
+        if(col.gameObject.layer==8){
+            Debug.Log("has entered col");
+            uiObj.SetActive(true);
+        }
+    }
+ void OnTriggerExit2D(Collider2D col){
+        if(col.gameObject.layer==8){
+            uiObj.SetActive(false);
+        }
+    }
+
+
+}
