c322521aa7413c51f04d1ad5eeb4c81e35c5ed28
lacee <laci96zenta@gmail.com>
2019-12-01 10:31:29 +0100

    waaaaaa

--- Assets/Code/Player/PlayerStats.cs
+++ Assets/Code/Player/PlayerStats.cs
@@ -5,18 +5,29 @@ public class PlayerStats : MonoBehaviour
 {
 	GameState gs;
 	PlayerController pc;
+	public float maxHP = 100.0f;
 	public float playerHP = 100.0f;
+	public GameObject UIObject;
 	// Start is called before the first frame update
 	void Start()
     {
 		pc = GetComponent<PlayerController>();
 		gs = GameState.GetGameState();
+		UIObject = GameObject.FindGameObjectWithTag("GreenBar");
 		UpdateUI();
 	}
 
 	private void UpdateUI()
 	{
-		//throw new NotImplementedException();
+
+		var rect = UIObject.GetComponent<RectTransform>();
+		var newScale = rect.localScale;
+		newScale.x = playerHP / maxHP;
+		if(playerHP <= 0)
+		{
+			newScale.x = 0;
+		}
+		rect.localScale = newScale;
 	}
 
 	// Update is called once per frame
